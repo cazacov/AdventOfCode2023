de78fe0354269905066a5d812416a1ec470faf4c
Victor Cazacov <cazacov.git@gmail.com>
2023-12-07 10:50:57 +0100

    Solve day 7 puzzles

---
+++ CSharp/Day07/Hand.cs
@@ -0,0 +1,104 @@
+﻿using System.Diagnostics;
+
+namespace Day07;
+
+public enum Strength
+{
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind
+}
+
+[DebuggerDisplay("{SourceCards} ")]
+public class Hand
+{
+
+    public string SourceCards;
+    public string Cards;
+    public long Bid;
+    public Strength Strength;
+
+    public Hand(string input, bool useJokers)
+    {
+        var parts = input.Split(" ");
+        this.Bid = long.Parse(parts[1]);
+        this.SourceCards = parts[0];
+        this.Cards = this.SourceCards
+            .Replace("T", "C")
+            .Replace("J", "D")
+            .Replace("Q", "E")
+            .Replace("K", "F")
+            .Replace("A", "G");
+
+        if (!useJokers || !this.Cards.Contains("D")) // Does not contain Joker
+        {
+            this.Strength = GetStrength(this.Cards);
+        }
+        else
+        {
+            var maxStrength = Strength.HighCard;
+            foreach (var joker in "23456789CDEFG")
+            {
+                var cards = this.Cards.Replace("D", joker.ToString());
+                var strength = GetStrength(cards);
+                if (strength >= maxStrength)
+                {
+                    maxStrength = strength;
+                }
+            }
+
+            this.Strength = maxStrength;
+            this.Cards = this.Cards.Replace("D", "0");
+        }
+    }
+
+    private static Strength GetStrength(string cards)
+    {
+        Strength result;
+
+        // Count card occurrences
+        var dict = cards
+            .GroupBy(x => x)
+            .ToDictionary(x => x.Key, x => x.Count());
+
+        // Calculate strength
+        if (dict.Any(x => x.Value == 5))
+        {
+            result = Strength.FiveOfAKind;
+        }
+        else if (dict.Any(x => x.Value == 4))
+        {
+            result = Strength.FourOfAKind;
+        }
+        else if (dict.Any(x => x.Value == 3) && dict.Count == 2)
+        {
+            result = Strength.FullHouse;
+        }
+        else if (dict.Any(x => x.Value == 3) && dict.Count == 3)
+        {
+            result = Strength.ThreeOfAKind;
+        }
+        else if (dict.Count == 3 && dict.Count(x => x.Value == 2) == 2)
+        {
+            result = Strength.TwoPair;
+        }
+        else if (dict.Count(x => x.Value == 2) == 1 && dict.Count(x => x.Value == 1) == 3)
+        {
+            result = Strength.OnePair;
+        }
+        else if (dict.Count == 5)
+        {
+            result = Strength.HighCard;
+        }
+        else
+        {
+            throw new NotImplementedException();
+        }
+
+        return result;
+    }
+}
---
+++ CSharp/Day07/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Day07
+{
+    internal partial class Program
+    {
+
+        static void Main()
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            var hands = input.Select(line => new Hand(line, false)).ToList();
+            Console.WriteLine($"Puzzle 1: {CalculateWinnings(hands)}");
+
+            hands = hands = input.Select(line => new Hand(line, true)).ToList();
+            Console.WriteLine($"Puzzle 2: {CalculateWinnings(hands)}");
+        }
+
+        private static long CalculateWinnings(List<Hand> hands)
+        {
+
+            long result = 0;
+
+            hands.Sort(Comparison);
+            for (int i = 0; i < hands.Count; i++)
+            {
+                var rank = i + 1;
+                result += hands[i].Bid * rank;
+            }
+
+            return result;
+        }
+
+        private static int Comparison(Hand first, Hand second)
+        {
+            if (first.Strength != second.Strength)
+            {
+                var s1 = (int)first.Strength;
+                var s2 = (int)second.Strength;
+
+                return s1.CompareTo(s2);
+            }
+            else
+            {
+                return string.Compare(first.Cards, second.Cards);
+            }
+        }
+    }
+}
