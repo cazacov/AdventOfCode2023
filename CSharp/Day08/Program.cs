e35e10dcea5451b601879494f2be7b0337f9009e
Victor Cazacov <cazacov.git@gmail.com>
2023-12-09 06:54:26 +0100

    Refactoring

--- CSharp/Day08/Program.cs
+++ CSharp/Day08/Program.cs
@@ -1,7 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Text.RegularExpressions;
-
-namespace Day08
+﻿namespace Day08
 {
     internal class Program
     {
@@ -10,103 +7,10 @@ namespace Day08
         static void Main()
         {
             var input = File.ReadAllLines("input.txt");
-            var (Path, nodes) = ParseInput(input);
+            var (path, nodes) = ParseInput(input);
 
-            //Puzzle1(Path, nodes);
-            Puzzle2(Path, nodes);
-        }
-
-        private static void Puzzle2(string path, Dictionary<string, Node> nodes)
-        {
-            var currentNodes = nodes.Keys.Where(k => k[2] == 'A').ToArray();
-
-            var nn = currentNodes.Length;
-            long pathLength = path.Length;
-
-            long[] starts = new long[currentNodes.Length];
-            List<long>[] loops = new List<long>[currentNodes.Length];
-
-            for (int i = 0; i < nn; i++)
-            {
-                var n = 0L;
-                var current = currentNodes[i];
-
-                var known = new Dictionary<string, long>();
-                var key = current + (n % pathLength);
-
-                while (true)
-                {
-                    FindNext(ref current, ref n, path, nodes);
-                    key = current + (n % pathLength);
-                    if (known.ContainsKey(key))
-                    {
-                        starts[i] = known[key];
-
-                        var pairs = known.Where(pair => pair.Value > starts[i]).Select(x => x.Value).OrderBy(x => x).ToList();
-                        var c = starts[i];
-                        var loop = new List<long>();
-                        foreach (var p in pairs)
-                        {
-                            loop.Add(p - c);
-                            c = p;
-                        }
-                        loops[i] = loop;
-                        break;
-                    }
-                    known[key] = n;
-                }
-            }
-
-            var res = starts[0];
-            foreach (var s in starts.Skip(1))
-            {
-                res = determineLCM(res, s);
-            }
-
-            Console.WriteLine(res);
-
-
-        }
-
-        public static long determineLCM(long a, long b)
-        {
-            long num1, num2;
-            if (a > b)
-            {
-                num1 = a; num2 = b;
-            }
-            else
-            {
-                num1 = b; num2 = a;
-            }
-
-            for (int i = 1; i < num2; i++)
-            {
-                long mult = num1 * i;
-                if (mult % num2 == 0)
-                {
-                    return mult;
-                }
-            }
-            return num1 * num2;
-        }
-
-        private static void FindNext(ref string current, ref long n, string path, Dictionary<string, Node> nodes)
-        {
-            do
-            {
-                var dir = path[(int)(n % path.Length)];
-                if (dir == 'L')
-                {
-                    current = nodes[current].Left;
-                }
-                else
-                {
-                    current = nodes[current].Right;
-                }
-
-                n++;
-            } while (current[2] != 'Z');
+            Puzzle1(path, nodes);
+            Puzzle2(path, nodes);
         }
 
         private static void Puzzle1(string path, Dictionary<string, Node> nodes)
@@ -117,34 +21,85 @@ namespace Day08
             while (current != "ZZZ")
             {
                 var dir = path[(int)(n % path.Length)];
-                if (dir == 'L')
-                {
-                    current = nodes[current].Left;
-                }
-                else
-                {
-                    current = nodes[current].Right;
-                }
+                current = dir == 'L' ? nodes[current].Left : nodes[current].Right;
                 n++;
             }
             Console.WriteLine(n);
         }
 
-        private static (string Path, Dictionary<String, Node> nodes) ParseInput(string[] input)
+        private static void Puzzle2(string path, Dictionary<string, Node> nodes)
+        {
+            var currentNodes = nodes.Keys.Where(k => k[2] == 'A').ToArray();
+
+            var nn = currentNodes.Length;
+            long pathLength = path.Length;
+
+            var starts = new long[currentNodes.Length];
+            var loops = new List<long>[currentNodes.Length];
+
+            for (var i = 0; i < nn; i++)
+            {
+                var n = 0L;
+                var current = currentNodes[i];
+
+                FindNext(ref current, ref n, path, nodes);
+                starts[i] = n;
+            }
+
+            var res = starts[0];
+            foreach (var s in starts.Skip(1))
+            {
+                res = DetermineLCM(res, s);
+            }
+
+            Console.WriteLine(res);
+        }
+
+        private static void FindNext(ref string current, ref long n, string path, Dictionary<string, Node> nodes)
+        {
+            do
+            {
+                var dir = path[(int)(n % path.Length)];
+                current = dir == 'L' ? nodes[current].Left : nodes[current].Right;
+                n++;
+            } while (current[2] != 'Z');
+        }
+
+        /// <summary>
+        /// Least common multiple
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static long DetermineLCM(long a, long b)
+        {
+            if (a < b)
+            {
+                (a, b) = (b, a);
+            }
+
+            for (var i = 1; i < b; i++)
+            {
+                var mult = a * i;
+                if (mult % b == 0)
+                {
+                    return mult;
+                }
+            }
+            return a * b;
+        }
+
+        private static (string Path, Dictionary<string, Node> nodes) ParseInput(string[] input)
         {
             var path = input[0];
             var nodes = new Dictionary<String, Node>();
 
-            int i = 2;
-            while (i < input.Length)
+            foreach (var line in input.Skip(2))
             {
-                var line = input[i];
                 var key = line.Substring(0, 3);
                 var left = line.Substring(7, 3);
                 var right = line.Substring(12, 3);
-
                 nodes[key] = new Node(left, right);
-                i++;
             }
             return (path, nodes);
         }
