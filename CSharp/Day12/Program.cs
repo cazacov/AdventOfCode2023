86f1860219d2aea928629777fd29a6741f12f812
Victor Cazacov <cazacov.git@gmail.com>
2023-12-13 05:31:58 +0100

    Add magic

--- CSharp/Day12/Program.cs
+++ CSharp/Day12/Program.cs
@@ -1,16 +1,21 @@
-﻿// 1087844863967
-
-
-namespace Day12
+﻿namespace Day12
 {
     internal class Program
     {
+        public record Record(string Springs, List<int> Data);
 
+        public class Descriptor(int lineNo, string lineSource, string line, List<int> data)
+        {
+            public int lineNo { get; set; } = lineNo;
+            public string lineSource { get; set; } = lineSource;
+            public string line { get; set; } = line;
+            public List<int> Data { get; set; } = data;
+            public Int128 matched { get; set; }
+        }
 
         static void Main()
         {
             var lines = File.ReadAllLines("input.txt");
-
             var records = ParseInput(lines);
 
             Puzzle1(records);
@@ -31,40 +36,124 @@ namespace Day12
             return result;
         }
 
-        public record Record(string springs, List<int> data);
-
-        public class Descriptor(int lineNo, string lineSource, string line, List<int> data)
+        private static void Puzzle1(List<Record> records)
         {
-            public int lineNo { get; set; } = lineNo;
-            public string lineSource { get; set; } = lineSource;
-            public string line { get; set; } = line;
-
-            public List<int> Data { get; set; } = data;
-            public long matched { get; set; }
+            var res = 0L;
+            foreach (var record in records)
+            {
+                res += CountArrangements(record);
+            }
+            Console.WriteLine($"Puzzle 1: {res} ");
         }
+
+        private static long CountArrangements(Record record)
+        {
+            var springs = record.Springs;
+            var data = new List<int>(record.Data);
+
+            var unknowns = new List<int>();
+            for (int i = 0; i < springs.Length; i++)
+            {
+                if (springs[i] == '?')
+                {
+                    unknowns.Add(i);
+                }
+            }
+
+            long matches = 0;
+
+            for (int mask = 0; mask < 1 << unknowns.Count; mask++)
+            {
+                var sp = new bool[springs.Length];
+                var unkPtr = 0;
+                for (int i = 0; i < springs.Length; i++)
+                {
+                    if (springs[i] == '#')
+                    {
+                        sp[i] = false;
+                    }
+                    else if (springs[i] == '.')
+                    {
+                        sp[i] = true;
+                    }
+                    else
+                    {
+                        sp[i] = (mask & (1 << unkPtr)) != 0;
+                        unkPtr++;
+                    }
+                }
+
+                if (MatchesPattern(sp, data))
+                {
+                    matches++;
+                }
+            }
+            return matches;
+        }
+
+        private static bool MatchesPattern(bool[] springs, List<int> data)
+        {
+            List<int> starts = new List<int>();
+            List<int> ends = new List<int>();
+
+            var n = springs.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (!springs[i]
+                    && (i == 0 || (i > 0 && springs[i - 1]))
+                    )
+                    starts.Add(i);
+
+                if (springs[i] && (i > 0 && !springs[i - 1]))
+                {
+                    ends.Add(i);
+                }
+            }
+
+            if (!springs[n - 1])
+            {
+                ends.Add(n);
+            }
+
+
+            if (data.Count != starts.Count || data.Count != ends.Count)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (data[i] != ends[i] - starts[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
 
         private static void Puzzle2(List<Record> records)
         {
             var descriptors = new List<Descriptor>();
 
-            var res = 0L;
             for (int i = 0; i < records.Count; i++)
             {
                 var record = records[i];
-                var springs = record.springs + "?" + record.springs + "?" + record.springs + "?" + record.springs +
-                              "?" + record.springs;
+                var springs = record.Springs + "?" + record.Springs + "?" + record.Springs + "?" + record.Springs +
+                              "?" + record.Springs;
                 var data = new List<int>();
-                data.AddRange(record.data);
-                data.AddRange(record.data);
-                data.AddRange(record.data);
-                data.AddRange(record.data);
-                data.AddRange(record.data);
+                data.AddRange(record.Data);
+                data.AddRange(record.Data);
+                data.AddRange(record.Data);
+                data.AddRange(record.Data);
+                data.AddRange(record.Data);
 
-                descriptors.Add(new Descriptor(i,  record.springs, springs, data));
+                descriptors.Add(new Descriptor(i,  record.Springs, springs, data));
             }
 
- 
-            long count = 0;
+            Int128 count = 0;
             Parallel.ForEach(descriptors, item =>
                 {
                     CountArrangements4(item);
@@ -72,15 +161,27 @@ namespace Day12
                     count++;
                 }
             );
-            Console.WriteLine($"Puzzle 2: {descriptors.Sum(item => item.matched)}");
+
+            var res = Int128.Zero;
+            foreach (var item in descriptors)
+            {
+                res += item.matched;
+            }
+            Console.WriteLine($"Puzzle 2: {res}");
         }
 
-        public record Span(string Pattern, long Cardinality);
+        public record Span(string Pattern, Int128 Cardinality);
         
-        private static long CountArrangements4(Descriptor descriptor)
+        private static Int128 CountArrangements4(Descriptor descriptor)
         {
             var springs = descriptor.line;
             var data = descriptor.Data;
+
+            if (descriptor.line.All(c => c == '?'))
+            {
+                descriptor.matched = Magic(descriptor.line.Length, descriptor.Data);
+                return descriptor.matched;
+            }
 
             var spans = new List<List<Span>>();
 
@@ -130,14 +231,42 @@ namespace Day12
 
             int spanIndex = 0;
             int datIndex = 0;
-
-            
             var result = CheckPlus(spans, data, spanIndex, datIndex, false, false);
             descriptor.matched = result;
             return result;
         }
 
-        private static long CheckPlus(List<List<Span>> spans, List<int> data, int spanIndex, int datIndex,
+        private static Int128 Magic(int lineLength, List<int> data)
+        {
+            var d = data.Sum() - data.Count();
+            var n = lineLength - d;
+            var k = data.Count();
+
+            n -= k - 1;
+
+            var res = Int128.One;
+            for (int i = 0; i < n - k; i++)
+            {
+                res *= n - i;
+            }
+
+            res /= Factorial(n - k);
+            return res;
+        }
+
+        private static Int128 Factorial(int n)
+        {
+            var res = Int128.One;
+
+            for (int i = 2; i <= n; i++)
+            {
+                res *= i;
+            }
+            return res;
+        }
+
+
+        private static Int128 CheckPlus(List<List<Span>> spans, List<int> data, int spanIndex, int datIndex,
             bool startWithHash, bool startWithDot)
         {
             if (spanIndex == spans.Count)
@@ -150,7 +279,7 @@ namespace Day12
             }
 
 
-            long result = 0;
+            Int128 result = 0;
             
             foreach (var span in spans[spanIndex])
             {
@@ -219,12 +348,17 @@ namespace Day12
         {
             var variants = new List<string>();
 
-            for (int mask = 0; mask < 1 << unknownLen; mask++)
+            if (unknownLen > 30)
+            {
+                Console.WriteLine("OMFG");
+            }
+
+            for (long mask = 0; mask < 1L << unknownLen; mask++)
             {
                 var str = "" + previous;
                 for (int bit = 0; bit < unknownLen; bit++)
                 {
-                    if ((mask & (1 << bit)) == 0)
+                    if ((mask & (1L << bit)) == 0)
                     {
                         str += '.';
                     }
@@ -275,134 +409,6 @@ namespace Day12
             }
 
             return ranges;
-        }
-
-
-        private static void Puzzle1(List<Record> records)
-        {
-            /*
-            var res = 0L;
-            foreach (var record in records)
-            {
-                res += CountArrangements(record);
-            }
-            Console.WriteLine($"Puzzle 1: {res} ");*/
-
-            var descriptors = new List<Descriptor>();
-
-            var res = 0L;
-            for (int i = 0; i < records.Count; i++)
-            {
-                var record = records[i];
-                var springs = record.springs
-                              //+ "?" + record.springs + "?" + record.springs + "?" + record.springs + "?" + record.springs
-                              ;
-                var data = new List<int>();
-                data.AddRange(record.data);
-                //data.AddRange(record.data);
-                //data.AddRange(record.data);
-                //data.AddRange(record.data);
-                //data.AddRange(record.data);
-
-                descriptors.Add(new Descriptor(i, record.springs, springs, data));
-            }
-
-
-            long count = 0;
-            Parallel.ForEach(descriptors, item =>
-                {
-                    CountArrangements4(item);
-                    Console.WriteLine($"{count,3} - {item.lineNo,3} - {item.lineSource,-35} {item.matched,15}");
-                    count++;
-                }
-            );
-            Console.WriteLine($"Puzzle 1: {descriptors.Sum(item => item.matched)}");
-        }
-
-        private static long CountArrangements(Record record)
-        {
-            var springs = record.springs;
-            var data = new List<int>(record.data);
-
-            var unknowns = new List<int>();
-            for (int i = 0; i < springs.Length; i++)
-            {
-                if (springs[i] == '?')
-                {
-                    unknowns.Add(i);
-                }
-            }
-
-            long matches = 0;
-
-            for (int mask = 0; mask < 1 << unknowns.Count; mask++)
-            {
-                var sp = new bool[springs.Length];
-                var unkPtr = 0;
-                for (int i = 0; i < springs.Length; i++)
-                {
-                    if (springs[i] == '#')
-                    {
-                        sp[i] = false;
-                    }
-                    else if (springs[i] == '.')
-                    {
-                        sp[i] = true;
-                    }
-                    else
-                    {
-                        sp[i] = (mask & (1 << unkPtr)) != 0;
-                        unkPtr++;
-                    }
-                }
-
-                if (MatchesPattern(sp, data))
-                {
-                    matches++;
-                }
-            }
-            return matches;
-        }
-
-        private static bool MatchesPattern(bool[] springs, List<int> data)
-        {
-            List<int> starts = new List<int>();
-            List<int> ends = new List<int>();
-
-            var n = springs.Length;
-
-            for (int i = 0; i < n; i++)
-            {
-                if (!springs[i] 
-                    && (i == 0 || (i > 0 && springs[i-1]))
-                    )
-                    starts.Add(i);
-
-                if (springs[i] && (i > 0 && !springs[i - 1]))
-                {
-                    ends.Add(i);
-                }
-            }
-
-            if (!springs[n - 1])
-            {
-                ends.Add(n);
-            }
-
-
-            if (data.Count != starts.Count || data.Count != ends.Count)
-            {
-                return false;
-            }
-
-            for (int i = 0; i < data.Count; i++)
-            {
-                if (data[i] != ends[i] - starts[i])
-                {
-                    return false;
-                }
-            }
-            return true;
         }
     }
 }
